09ca8adde7948d2ee5331640c55bf2a9f746af9c
AdamMintaj <maliszewski.aa@gmail.com>
2021-02-21 21:01:46 +0100

    initializing repository

---
+++ script.js
@@ -0,0 +1,23 @@
+var accordion = document.getElementsByClassName("question");
+
+for (i=0; i < accordion.length; i++)
+{
+	accordion[i].addEventListener("click", function()
+	{
+		if(this.classList.contains("question_active"))
+		{
+			this.classList.toggle("question_active");
+			this.classList.add("question_closed");
+		}
+		else
+		{
+			for(i=0; i< accordion.length; i++)
+			{
+				accordion[i].classList.remove("question_active");	
+				accordion[i].classList.add("question_closed");	
+			}
+			this.classList.toggle("question_active");
+			this.classList.remove("question_closed");
+		}
+	});
+}
